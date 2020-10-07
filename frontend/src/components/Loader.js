b818724ad905e6c1776772c4e08cc0b6d39a041d
HassanKhan123 <hk2602809@gmail.com>
2020-10-07 03:31:26 -0700

    message and loader component added

---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+
+const Loader = () => {
+  return (
+    <Spinner
+      animation='border'
+      role='status'
+      style={{
+        width: 100,
+        height: 100,
+        margin: 'auto',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <span className='sr-only'>Loading...</span>
+    </Spinner>
+  );
+};
+
+export default Loader;
