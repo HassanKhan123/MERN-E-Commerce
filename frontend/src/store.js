62281d182b7f990e505cd0eb45d9f0767cc2e9b9
HassanKhan123 <hk2602809@gmail.com>
2020-10-07 02:45:17 -0700

    productList reducer and action created

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducers = combineReducers({});
+import { productsListReducer } from './reducers/productReducers';
+
+const reducers = combineReducers({
+  productList: productsListReducer,
+});
 const initialState = {};
 const middlewares = [thunk];
 
