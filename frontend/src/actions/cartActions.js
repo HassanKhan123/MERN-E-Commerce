c4d4bfbcea9c87cdb9a4c1bf79314b73bd78b3fa
HassanKhan123 <hk2602809@gmail.com>
2020-10-11 23:00:36 +0500

    remove items from cart functionality added

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -21,3 +21,15 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
     console.log(error);
   }
 };
+
+export const removeFromCart = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: CART_REMOVE_ITEM,
+      payload:id
+    });
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+  } catch (error) {
+    console.log(error);
+  }
+};
