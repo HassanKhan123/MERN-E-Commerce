6283c248ccf0fa44072f9581e3450c8477bbdef5
HassanKhan123 <hk2602809@gmail.com>
2020-10-13 14:38:12 +0500

    user authentication end point created

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,17 @@ import colors from 'colors';
 
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js'
 import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json())
 
 app.use('/api/products', productRoutes);
+app.use('/api/users',userRoutes)
 
 app.use(notFound);
 
