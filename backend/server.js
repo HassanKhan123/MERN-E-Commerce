c07b54aea0d39815db0e4a6b32af6d71b3e5a475
HassanKhan123 <hk2602809@gmail.com>
2020-10-15 18:57:46 +0500

    order controller and route added

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,19 @@ import colors from 'colors';
 
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.use('/api/products', productRoutes);
-app.use('/api/users',userRoutes)
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 
